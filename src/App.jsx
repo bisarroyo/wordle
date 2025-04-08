c4b68882a3b23feeb71d7fc286cfdf1cc4fc576d
Bismark Arroyo <bis.arroyo@gmail.com>
2025-04-08 15:40:14 -0600

    add logic for hadle words

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import Line from './components/Line'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [solution, setSolution] = useState('hello')
+  const [current, setCurrent] = useState('')
+  const [wordle, setWordle] = useState(new Array(6).fill(null))
+  const [isGameOver, setIsGameOver] = useState(false)
+  useEffect(() => {
+    setSolution('hello')
+  }, [])
 
+  useEffect(() => {
+    function onKeyUp(event) {
+      if (isGameOver) {
+        return
+      }
+      const { key } = event
+      if (key == 'Enter') {
+        if (current.length !== 5) {
+          return
+        }
+        const isCorrect = solution === current
+        if (isCorrect) {
+          setIsGameOver(true)
+        }
+        const newWordle = [...wordle]
+        const actualWordleIndex = wordle.findIndex((val) => val == null)
+        newWordle[actualWordleIndex] = current
+        setWordle(newWordle)
+        setCurrent('')
+      }
+      if (key == 'Backspace') {
+        setCurrent((prevState) => prevState.slice(0, -1))
+        return
+      }
+      if (current.length >= 5) {
+        return
+      }
+
+      setCurrent((prevState) => (prevState += key))
+    }
+    window.addEventListener('keydown', onKeyUp)
+
+    return () => {
+      window.removeEventListener('keydown', onKeyUp)
+    }
+  }, [current, solution, wordle, isGameOver])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Wordle</h1>
+      {wordle.map((word, index) => {
+        const isCurrent = index === wordle.findIndex((val) => val == null)
+        return (
+          <Line
+            key={index}
+            currentTyping={isCurrent ? current : word ?? ''}
+            solution={solution}
+            isLastWord={wordle[index] !== null && !isCurrent}
+          />
+        )
+      })}
+      {isGameOver && 'Game Over'}
+    </main>
   )
 }
 
