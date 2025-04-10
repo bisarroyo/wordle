35cdd0eb7e3138920cc77db0f68f045d99a5c67e
Bismark Arroyo <bis.arroyo@gmail.com>
2025-04-10 13:56:59 -0600

    add options component for submit and next

---
+++ src/components/Options.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Options({ onKeyPressed }) {
+  return (
+    <div className='options'>
+      <button
+        className='option'
+        onClick={(e) => {
+          e.currentTarget.blur()
+          onKeyPressed('Enter')
+        }}>
+        Submit
+      </button>
+      <button
+        className='option'
+        onClick={(e) => {
+          e.currentTarget.blur()
+          onKeyPressed('Next')
+        }}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='currentColor'
+          className='icon icon-tabler icons-tabler-filled icon-tabler-player-track-next'>
+          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+          <path d='M2 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z' />
+          <path d='M13 5v14c0 .86 1.012 1.318 1.659 .753l8 -7a1 1 0 0 0 0 -1.506l-8 -7c-.647 -.565 -1.659 -.106 -1.659 .753z' />
+        </svg>
+      </button>
+    </div>
+  )
+}
+
+export default Options
